9e44a76761bd074031c8fc085d500eba4da5470b
Ross Henderson <rossliamhenderson@gmail.com>
2023-01-03 22:52:54 +0000

    2 Hours debugging why my CSS webpack Loader wasn't configuring until I figured out the name of my webpack configure file was spelled wrong.
    Got there though!
    
    Now everything is configured correctly.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import './style.css';
+
 let main = document.getElementById('content');
+main.classList='mainBackground';
 let header = document.createElement('div');
+
+let pizzaImg = document.createElement('img');
+pizzaImg.src = '../img/pizza.jpg'
+
+header.appendChild(pizzaImg);
+
+let title = document.createElement('h1');
+title.innerHTML='Test';
+
+main.appendChild(title);
 main.appendChild(header);
