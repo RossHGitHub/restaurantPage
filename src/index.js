3cffe4789017de7267463afb7c90014a89e19082
Ross Henderson <rossliamhenderson@gmail.com>
2023-01-04 22:50:56 +0000

    Finished static styling. added page content into pageload module.
    Now loading from pageload module via exported function.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
 import './style.css';
+import { pageload } from './pageload';
 
-let main = document.getElementById('content');
-main.classList='mainBackground';
-let header = document.createElement('div');
-
-let pizzaImg = document.createElement('img');
-pizzaImg.src = '../img/pizza.jpg'
-
-header.appendChild(pizzaImg);
-
-let title = document.createElement('h1');
-title.innerHTML='Test';
-
-main.appendChild(title);
-main.appendChild(header);
+pageload();
---
+++ src/pageload.js
@@ -0,0 +1,63 @@
+    export function pageload(){// Background, HTML and Body tags
+    let body = document.querySelector('body');
+    body.classList='body';
+    
+    let html = document.querySelector('html');
+    html.classList='body';
+    
+    let main = document.getElementById('content');
+    main.classList='mainBackground';
+    
+    
+    
+        // Header
+    let header = document.createElement('div');
+    header.classList='header';
+    main.appendChild(header);
+    
+    let headerTitle = document.createElement('h1');
+    headerTitle.innerHTML="Bapa Daboopi's!!";
+    header.appendChild(headerTitle);
+    let headerSubtitle = document.createElement('h3');
+    headerSubtitle.innerHTML="The finest pizzeria this side of your-a mamma mia!";
+    header.appendChild(headerSubtitle);
+    
+    
+    
+        // Main Content Wrapper
+    
+    let mainContentWrapper = document.createElement('div');
+    mainContentWrapper.classList='mainContentWrapper';
+    main.appendChild(mainContentWrapper);
+    
+    
+        // Main Content Left
+    
+    let mainContentLeft= document.createElement('div');
+    mainContentLeft.classList='mainContentLeft';
+    mainContentWrapper.appendChild(mainContentLeft);
+    
+    let MCLImageOne = document.createElement('img')
+    MCLImageOne.src='../img/pastaOne.jpg'
+    MCLImageOne.classList='MCLImage';
+    
+    let MCLImageTwo = document.createElement('img')
+    MCLImageTwo.src='../img/pizza.jpg';
+    MCLImageTwo.classList='MCLImage';
+    
+    let MCLImageThree = document.createElement('img')
+    MCLImageThree.src='../img/pastaTwo.jpg';
+    MCLImageThree.classList='MCLImage';
+    
+    mainContentLeft.appendChild(MCLImageOne);
+    mainContentLeft.appendChild(MCLImageTwo);
+    mainContentLeft.appendChild(MCLImageThree);
+    
+    
+    
+        // Main Content Right
+    
+    let mainContentRight = document.createElement('div');
+    mainContentRight.classList = 'mainContentRight';
+    mainContentWrapper.appendChild(mainContentRight);
+    }
